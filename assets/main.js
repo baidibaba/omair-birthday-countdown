9d4efb89c9f2006f0a00c8fb910efd94b8b4de66
baidibaba <theubaidullah@gmail.com>
2024-01-26 20:48:42 +0500

    button added

--- assets/main.js
+++ assets/main.js
@@ -49,10 +49,14 @@ function updateCountdown() {
 
   if (April) {
     console.log('Happy Birthday Omair')
-    message.innerHTML = '<br> Guys and Gals, It gives me great pleasure to notify you that today is Omair birthday.<br>Happy Birthday!!'
+    message.innerHTML = `<br> Guys and Gals, It gives me great pleasure to notify you that today is Omair birthday.<br>Happy Birthday!! Click this button now Omair <br/>
+    <button  class="bg-green-950 text-green-400 border border-green-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+  <span class="bg-green-400 shadow-green-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+  <a href='https://omair-birthday.vercel.app/'>Click Me</a></button>
+    `
     countdown.style.display = "none"
     birthdayTime.style.display = "none"
-    yearold.innerText = `${age+1}th Birthday!`;
+    yearold.innerText = `${age}th Birthday!`;
     clearInterval(timerId)
     return
   }
